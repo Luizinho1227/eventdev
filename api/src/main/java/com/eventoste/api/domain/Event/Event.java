33d961bc97c1d2c4fedc4bea9d47ffd020ddf597
Luizinho1227 <luizfelipe3333lp@gmail.com>
2024-07-01 22:55:14 -0300

    primeiro commit

---
+++ api/src/main/java/com/eventoste/api/domain/Event/Event.java
@@ -0,0 +1,33 @@
+package com.eventoste.api.domain.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name= "envet" )
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+
+}
